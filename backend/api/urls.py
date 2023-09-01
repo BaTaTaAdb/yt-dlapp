5be1a23b51a1f85ad3df9c5fa41c3f136eed9682
BaTaTaAdb <joaomnuno@gmail.com>
2023-09-01 20:21:04 +0100

    added url for video link create view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import VideoListCreateView, VideoRetrieveUpdateDestroyView
+from .views import VideoListCreateView, VideoRetrieveUpdateDestroyView, VideoLinkCreateView
 
 urlpatterns = [
     path('videos/', VideoListCreateView.as_view(), name='video-list-create'),
     path('videos/<int:pk>/', VideoRetrieveUpdateDestroyView.as_view(),
          name='video-retrieve-update-destroy'),
-
+    path('videos/link/', VideoLinkCreateView.as_view(), name='video-link-create'),
 ]
