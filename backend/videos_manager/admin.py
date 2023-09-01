cd2454c68d4af5eac256eab30da9267cca72574d
BaTaTaAdb <joaomnuno@gmail.com>
2023-09-01 11:28:29 +0100

    added Video and YTUser models

--- backend/videos_manager/admin.py
+++ backend/videos_manager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Video
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'upload_date', 'views', 'download', 'author')
+    search_fields = ('title', 'author__username')
