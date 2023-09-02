079b6c7dd4a8f572b946ef0344dfef1c69f3b7ca
BaTaTaAdb <joaomnuno@gmail.com>
2023-09-02 19:05:56 +0100

    ! NOT PRODUCTION READ SENDFILE CONFIG !

--- backend/yt_dlproject/settings.py
+++ backend/yt_dlproject/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# ! NOT PRODUCTION READY !
+SENDFILE_BACKEND = 'sendfile.backends.development'
